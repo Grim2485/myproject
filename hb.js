a295b7434afe8784e12100448861ed416d15c404
Grim2485 <k29267597@gmail.com>
2025-12-04 07:19:14 +0800

    hb修復

---
+++ hb.js
@@ -0,0 +1,8 @@
+const backdrop = document.querySelector(".backdrop")
+        const hb = document.querySelector(".hb")
+        backdrop.addEventListener("click", function () {
+            
+            
+            hb.children[0].checked = false;
+        })
+
